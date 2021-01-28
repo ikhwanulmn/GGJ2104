cf09616dc15cea1123fac7b28d6ce23f79371661
Rird <49355624+817r@users.noreply.github.com>
2021-01-28 10:37:56 +0700

    Cam Char Grav

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController Controller;
+
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    public Transform groundcheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        Controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        Controller.Move(velocity * Time.deltaTime);
+
+    }
+}
